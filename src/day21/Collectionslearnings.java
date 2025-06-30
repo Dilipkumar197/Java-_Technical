2da399ecacb9fd9040dc050a055be35844d95f41
Dilipkumar197 <vdilipkumar197@gmail.com>
2025-06-30 11:24:05 +0530

    Third changes

---
+++ src/day21/Collectionslearnings.java
@@ -0,0 +1,33 @@
+package day21;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Collectionslearnings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ArrayList mylist = new ArrayList();
+		mylist.add(100);
+		mylist.add("Dilip");
+		
+		System.out.println(mylist);
+		
+		HashSet myset = new HashSet();
+		myset.add(200);
+		myset.add("Deepu");
+		
+		System.out.println(myset);
+		
+		
+		HashMap hm = new HashMap();
+		hm.put(300 , "Teja");
+		
+		System.out.println(hm);
+		
+	}
+
+}
